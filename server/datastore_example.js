763aa333a310fdd8b3fbd2e679337f49c44bcf7c
Jessie Douglas <jessielou.douglas@gmail.com>
2020-01-14 11:10:35 -0500

    Update datastore to async/await

--- server/datastore_example.js
+++ server/datastore_example.js
@@ -7,15 +7,15 @@ const datastore = new Datastore();
 const BLOG_POST = 'blog_post';
 
 exports.insertBlogPost = async (blogPost) => {
-  const savedPostPromise = datastore.save({
+  const savedPost = await datastore.save({
     key: datastore.key(BLOG_POST),
     data: blogPost,
   });
-  return savedPostPromise;
+  return savedPost;
 };
 
 exports.getAllBlogPosts = async () => {
   const query = datastore.createQuery(BLOG_POST);
-  const allPostsPromise = datastore.runQuery(query);
-  return allPostsPromise;
+  const allPosts = await datastore.runQuery(query);
+  return allPosts;
 };
